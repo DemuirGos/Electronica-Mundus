5813a20dae7b0960249fa8f5c7d697503301c4b4
silverest12 <Silverest12@gmail.com>
2021-03-16 18:46:35 +0100

    cursors

--- curseurQ1.sql
+++ curseurQ1.sql
@@ -1,5 +1,11 @@
 DECLARE
-    c_Ref Produit.REF%type;    
+    prodId CATALOGUE.Ref%type;
+    CURSOR prodCurs IS
+        SELECT * FROM CATALOGUE WHERE REF = prodId;
 BEGIN
-
+    prodId :=: prodId;
+    FOR prod IN prodCurs
+    LOOP
+         DBMS_OUTPUT.PUT_LINE(prod.REF || ' | ' || prod.LIBELLE || ' | ' || prod.ID_CATEGORIE || ' | ' || prod.ID_FAMILLE || ' | ' || prod.PU || ' | ' || prod.TVA);
+    END LOOP;
 END;
