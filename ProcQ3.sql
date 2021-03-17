0ff616b40b26571fe38e19bccc0610594641c3ce
silverest12 <Silverest12@gmail.com>
2021-03-17 14:13:21 +0100

    changed procs

--- ProcQ3.sql
+++ ProcQ3.sql
@@ -10,7 +10,3 @@ AS
 BEGIN
     launchOrder(numCom,dateCom,reg,idClient,idProduit,quant, remise);
 END;
-
-BEGIN
-    launchOrderWithDiscount(723, '01/FEB/2021', 0, 2, ARRAY_OF_VARCHAR('A01', 'A02'), ARRAY_OF_INT(5, 3), 10);
-end;
