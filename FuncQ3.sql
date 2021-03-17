4f68a9851f8023fd03a7d65d4b532c81db3c2260
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2021-03-17 03:41:11 +0100

    Create FuncQ3.sql
    exprimenting with the Functions

---
+++ FuncQ3.sql
@@ -0,0 +1,43 @@
+DECLARE
+    CREATE OR REPLACE FUNCTION toCurrency(
+        montant IN Number,
+        ratio IN Number
+    )
+    RETURN Number AS result
+    BEGIN 
+        result := montant * ratio;
+        return result;
+    END; 
+
+    CREATE OR REPLACE FUNCTION toUSD(
+        montant IN Number,
+        ratio IN Number DEFAULT 9.00
+    )
+    RETURN Number AS result
+    BEGIN 
+        result := toCurrency(montant,ratio);
+        return result;
+    END; 
+
+    CREATE OR REPLACE FUNCTION toEUR(
+        montant IN Number,
+        ratio IN Number DEFAULT 10.71
+    )
+    RETURN Number AS result
+    BEGIN 
+        result := toCurrency(montant,ratio);
+        return result;
+    END; 
+CREATE OR REPLACE FUNCTION toUSDorEUR(
+    price IN Number,
+    currency IN Varchar2
+)
+RETURN Number AS result
+BEGIN 
+    IF currency = 'USD' THEN
+        result :=  toUSD(price)
+    ELSIF currency = 'EUR' THEN
+        result :=  toUSD(EUR)
+    END IF;
+    return result;
+END; 
