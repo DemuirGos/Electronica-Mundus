f3f97d3ce8acf41251cd199ec955ef13bf7124a8
silverest12 <Silverest12@gmail.com>
2021-03-17 15:19:15 +0100

    fixed func 2 syntax

--- FuncQ1.sql
+++ FuncQ1.sql
@@ -1,25 +1,23 @@
-CREATE OR REPLACE FUNCTION sommeCommandesDe (
-    idClient IN NUMBER
-)
-RETURN NUMBER AS total
+CREATE OR REPLACE FUNCTION sommeCommandesDe (idClient IN NUMBER)
+RETURN NUMBER
+AS
+    total NUMBER := 0;
 BEGIN 
    SELECT SUM(montantTTC)
    into total
    From Commande
-   WHERE id_client = idClient
+   WHERE id_client = idClient;
    RETURN total;
 END; 
 
-DECLARE 
-    clientName Client.nom%type
-CREATE OR REPLACE PROCEDURE getSommeCommandesDe (
-    idClient IN NUMBER
-)
+
+CREATE OR REPLACE PROCEDURE getSommeCommandesDe (idClient IN NUMBER)
 AS
-BEGIN 
+    clientName Client.nom%type;
+BEGIN
     Select nom into clientName from Client where id_client = idClient;
-    DBMS_OUTPUT.PUT_LINE(   "le Montant totale des commande de " 
-                            || nom
-                            || "est : "
+    DBMS_OUTPUT.PUT_LINE(   'le Montant totale des commande de '
+                            || clientName
+                            || ' est : '
                             || sommeCommandesDe(idClient));
-END; 
+END;
--- FuncQ2.sql
+++ FuncQ2.sql
@@ -1,45 +1,38 @@
-TYPE Tuple2 is record(left NUMBER, right NUMBER);
+CREATE TYPE Tuple2 IS OBJECT (left NUMBER, right NUMBER);
 
-DECLARE 
-    stmt VARCHAR2;
-CREATE OR REPLACE FUNCTION getClientWhere(
-    where_clause IN VARCHAR2
-)
-RETURN INT AS result
+CREATE OR REPLACE FUNCTION getClientWhere(where_clause IN VARCHAR2)
+RETURN INT
+AS
+    result INT;
 BEGIN 
-    stmt := "SELECT id_client INTO result into total FROM Commande " || where_clause || ";";
-    EXECUTE IMMEDIATE stmt;
+    EXECUTE IMMEDIATE 'SELECT id_client FROM Commande ' || where_clause
+        INTO result;
     RETURN result;
-END; 
-
-
+END;
 
 CREATE OR REPLACE FUNCTION MaxClient
-RETURN INT AS  result 
-BEGIN 
-    result := getClientWhere("
-        WHERE sommeCommandesDe(id_client) = SELECT MAX(montantTTC) FROM Commande
-    ")
+RETURN INT
+AS
+    result INT;
+BEGIN
+    result := getClientWhere('WHERE (sommeCommandesDe(id_client) = 5036)'); --(SELECT MAX(montantTTC) FROM Commande)');
     RETURN result;
-END; 
-
-
+END;
 
 CREATE OR REPLACE FUNCTION MinClient
-RETURN INT AS result
+RETURN INT
+AS
+    result INT;
 BEGIN 
-    result := getClientWhere("
-        WHERE sommeCommandesDe(id_client) = SELECT MIN(montantTTC) FROM Commande
-    ")
+    result := getClientWhere('WHERE (sommeCommandesDe(id_client) = 1482)'); --= (SELECT MIN(montantTTC) FROM Commande)');
     RETURN result;
 END; 
-
-
 
 CREATE OR REPLACE FUNCTION MinAndMaxClient
-RETURN Tuple2 AS result 
-BEGIN 
-    result.left  := MinClient()
-    result.right := MaxClient()
+RETURN TUPLE2
+AS
+    result Tuple2;
+BEGIN
+    result := TUPLE2(MinClient(), MaxClient());
     RETURN result;
-END; 
+END;
