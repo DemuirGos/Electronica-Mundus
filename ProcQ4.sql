2b70ef4e24dea4b40ff94677d2051eb95c672622
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-03-17 23:04:53 +0100

    Update ProcQ4.sql

--- ProcQ4.sql
+++ ProcQ4.sql
@@ -1,17 +1,16 @@
-CREATE OR REPLACE PROCEDURE launchOrderProdDisc
-            (numCom IN COMMANDE.N_COMMANDE%type,
-             dateCom IN COMMANDE.DATE_COMMANDE%TYPE,
-             reg IN COMMANDE.REGLE%TYPE,
-             idClient IN COMMANDE.ID_CLIENT%type,
-             idProduit IN  ARRAY_OF_VARCHAR,
-             quant IN ARRAY_OF_INT,
-             remises IN ARRAY_OF_INT)
+CREATE OR REPLACE PROCEDURE launchOrderProdDisc(
+             numCom IN COMMANDE.N_COMMANDE%type             ,
+             dateCom IN COMMANDE.DATE_COMMANDE%TYPE         ,
+             reg IN COMMANDE.REGLE%TYPE                     ,
+             idClient IN COMMANDE.ID_CLIENT%type            ,
+             idProduit IN  ARRAY_OF_VARCHAR                 ,
+             quant IN ARRAY_OF_INT                          ,
+             remises IN ARRAY_OF_INT                        )
 AS
 BEGIN
     INSERT INTO COMMANDE VALUES (numCom, dateCom, reg, idCLient, 0, 0);
     FOR i IN 1..idProduit.COUNT LOOP
         addProd(numCom, idProduit(i), quant(i), remises(i));
     END LOOP;
-
     updateOrder(numCom);
 END;
